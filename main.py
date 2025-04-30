7cb2a1958abeb8e9cb4f78b281bdb0049af97f2f
1363v4 <louisemmanuelgires@gmail.com>
2025-04-30 22:27:24 +0200

    dumb mega morph version

--- main.py
+++ main.py
@@ -1,14 +1,13 @@
 from quart import Quart, session, render_template, request
 from datastar_py.quart import make_datastar_response
 from datastar_py.sse import ServerSentEventGenerator as SSE
-
 from faker import Faker
-
 import asyncio
 import redis.asyncio as redis
 import json
 import random
 import time
+import uuid
 
 
 # CONFIG
@@ -22,79 +21,83 @@ fake = Faker()
 
 @app.before_serving
 async def start_redis():
-    global redis
-    redis = redis.Redis(
-        host="localhost",
-        port=6379,
-        db=0,
-        encoding="utf-8",
-        decode_responses=True,
-    )
+    global redis_client
+    redis_client = redis.Redis(host="localhost", port=6379, db=1, decode_responses=True)
 
 @app.after_serving
 async def close_redis():
-    await redis.close()
+    await redis_client.close()
 
 # APP STUFF
 
 @app.before_request
 async def before_request():
     if not session.get('user_id'): 
-        user_id = fake.name()
-        session['user_id'] = user_id
+        session['user_id'] = fake.name()
 
 @app.route('/')
 async def index():
-    return await render_template('index.html')
+    return await render_template('void.html')
 
 @app.route('/void')
 async def void():
-    user_id = session.get('user_id')
-
-    pubsub = redis.pubsub()
-    await pubsub.subscribe('main')
-    async def event(generator):
+    # pubsub = redis_client.pubsub()
+    # await pubsub.subscribe('main')
+    
+    async def event():
         while True:
             try:
-                ping = await pubsub.get_message()
-                messages = await redis.lindex("messages", 0)
-                html = '''
-                <div id="messages">
-                '''
-                for message in messages:
-                    payload = json.loads(message)
-                    html += f'''
-                    <div
-                    class="message"
-                    style="
-                        position:absolute;
-                        top:{payload.y}%;
-                        left:{payload.x}%;
-                        background:{payload.color};
-                    "
-                    </div>'''
-                yield SSE.merge_fragments(fragments=[html], use_view_transition=True)
+                # await pubsub.get_message()
+                keys = await redis_client.keys('msg-*')
+                if keys:
+                    html = '<div id="messages">'
+                    for key in keys:
+                        data = await redis_client.get(key)
+                        ttl = await redis_client.ttl(key)
+                        if data and ttl:
+                            opacity = ttl / 10
+                            payload = json.loads(data)
+                            html += f'''
+                            <div
+                            id="{key}"
+                            class="message"
+                            style="
+                                opacity:{opacity};
+                                top:{payload['y']}%;
+                                left:{payload['x']}%;
+                                background:{payload['color']};
+                            "
+                            >
+                            {payload['message']}
+                            </div>'''
+                    html += '</div>'
+                    yield SSE.merge_fragments(fragments=[html])
+                else:
+                    await asyncio.sleep(.2)
             except asyncio.CancelledError:
-                pubsub.unsubscribe('main')
+                # await pubsub.unsubscribe('main')
                 break
-    return await make_datastar_response(event)
+                
+    return await make_datastar_response(event())
 
 @app.post("/message")
 async def post_message():
     message = (await request.form).get('message')
     if not message:
-        return 200
-    color = f"#{random.randint(0, 0xFFFFFF):06x}"
-    x = round(random.random() * 100, 2)
-    y = round(random.random() * 100, 2)
+        return "No message", 200
+        
     payload = {
         "message": message,
-        "color": color,
-        "x": x,
-        "y": y,
+        "color": f"#{random.randint(0, 0xFFFFFF):06x}",
+        "x": round(10 + random.random() * 80, 2),
+        "y": round(5 + random.random() * 80, 2),
     }
-    await redis.lpush("messages", json.dumps(payload))
-    return 200
+    
+    msg_id = str(time.time()).replace(".", "-")
+    await redis_client.set(f"msg-{msg_id}", json.dumps(payload), ex=10)
+    # await redis_client.publish('main', msg_id)
+    
+    return "", 200
 
 if __name__ == '__main__':
     app.run(debug=True)
